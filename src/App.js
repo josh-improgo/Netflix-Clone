cd6b7fcf66c479bedd24e838ce2f7d4704ca088e
Josh Improgo <josh.improgo@gmail.com>
2021-03-30 01:42:20 -0400

    Netflix Clone code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import * as ROUTES from './constants/routes'
+import { Home, Browse, Signin, Signup } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+import { useAuthListener } from './hooks'
 
-function App() {
+export default function App() {
+  const { user } = useAuthListener()
+  console.log(user)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Switch>
+        {/* <Route exact path={ROUTES.SIGN_UP}> */}
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <Signup />
 
-export default App;
+        </IsUserRedirect>
+        {/* </Route> */}
+        {/* <Route exact path={ROUTES.SIGN_IN}> */}
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <Signin />
+        </IsUserRedirect>
+        {/* </Route> */}
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
+  )
+}
---
+++ src/pages/browse.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
+import  selectionFilter from '../utils/selection-filter'
+
+export default function Browse() {
+    const { series } = useContent('series')
+    const { films } = useContent('films')
+    // we need slides
+
+    const slides = selectionFilter({ series, films })
+    console.log(slides)
+    // pass it to the browse container
+
+    return (
+        <BrowseContainer slides={slides} />
+    )
+}
